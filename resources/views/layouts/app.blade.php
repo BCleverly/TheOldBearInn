687ec7ad5e5e4f259a463a750036cd2382aea441
Ben <ben@bencleverly.dev>
2020-09-23 20:01:23 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
 <div class="container">
 <div class="row">
 <div class="col">
-<div><h1>Covid-19 update:</h1>
+<div class="mb-4"><h1>Covid-19 update:</h1>
 <p>From Thursday 24<sup>th</sup> of September, we'll be opening from 4PM in the week.<br>If you're planning to visit for a sporting event (Football/Rugby etc) please get in touch to book a table.</p>
 </div>
 </div>
